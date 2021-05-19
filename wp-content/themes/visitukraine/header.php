b82e45e319db231da73d6d7ab1bf229f135c2c7f
evgeniy doronin <evgeniydoronin@gmail.com>
2021-05-19 19:30:09 +0300

    init project

--- wp-content/themes/visitukraine/header.php
+++ wp-content/themes/visitukraine/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <title>1Information page for tourists and visitors</title>
+    <title>Information page for tourists and visitors</title>
 
     <?php wp_head(); ?>
 </head>
